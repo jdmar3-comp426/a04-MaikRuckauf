1d838d0d628e1f8e65193bde2dedd67dcf021409
Maik Ruckauf <maik@ad.unc.edu>
2021-12-02 16:07:41 -0500

    fixed typo

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get("/app/", (req, res, next) => {
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
-app.post("/app/new/user", (req, res) => {
+app.post("/app/new/", (req, res) => {
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)").run(req.body.user,md5(req.body.pass));
 	res.status(201).json({"message":"1 record created: ID %id% (201)".replace("%id%", stmt.lastInsertRowid)});
 });
@@ -41,7 +41,7 @@ app.get("/app/user/:id", (req, res) => {
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").run(req.body.user,md5(req.body.pass),req.params.id);
-	res.status(201).json({"message":"1 record updated: ID %id% (201)".replace("%id%", req.params.id)});
+	res.status(201).json({"message":"1 record updated: ID %id% (200)".replace("%id%", req.params.id)});
 })
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 
